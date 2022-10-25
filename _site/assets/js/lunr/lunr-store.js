7b8598e7753c26d6f25b4cdc93f1e3c7956e2306
GrantsTracks2121 <60116625+GrantsTracks2121@users.noreply.github.com>
2022-10-25 09:32:41 -0600

    4

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,61 @@
+var store = [{
+        "title": "A new file with wikilinks",
+        "excerpt":" Get Ready for this:  Here’s a picture inserted with wikilink format !  [[  download.png  ]]   and links to other files:     your-first-note   move your body every day   consistency  ","categories": [],
+        "tags": [],
+        "url": "/a-new-file-with-wikilinks",
+        "teaser": null
+      },{
+        "title": "Another note",
+        "excerpt":" This is pretty special. You are looking at another note. If one note, wasn’t enough, right? Am I right???   Am I annoying?   Yet?   What’s wrong with this picture?     ","categories": [],
+        "tags": [],
+        "url": "/another-note",
+        "teaser": null
+      },{
+        "title": "Here's another page",
+        "excerpt":" While I wait for stuff to happen, I’m making another page. And I’m putting in links to My new digital garden and 안녕하세요 and just for the heck of it, move your body every day   Here’s some highlighted content.   ","categories": [],
+        "tags": [],
+        "url": "/here-s-another-page",
+        "teaser": null
+      },{
+        "title": "My new digital garden",
+        "excerpt":" Here are some musings, the point of the whole thing, remember! And that’s not all - there’s Another note I made TOO     ","categories": [],
+        "tags": [],
+        "url": "/my-new-digital-garden",
+        "teaser": null
+      },{
+        "title": "A note about cats",
+        "excerpt":"This is a second note with a poem with cats. Here’s a link to a note that explains why it’s important to move your body every day. I like my pillow, my fancy bed, My cat tree and the bathroom sink. Each has its time and fills a need, but...","categories": [],
+        "tags": [],
+        "url": "/cats",
+        "teaser": null
+      },{
+        "title": "Tigers",
+        "excerpt":" This is yet another note, this one about tigers.  ","categories": [],
+        "tags": [],
+        "url": "/tigers",
+        "teaser": null
+      },{
+        "title": "Consistency is key",
+        "excerpt":" Show up. Do the work. Be consistent.   Then go take a look at the first note.  ","categories": [],
+        "tags": [],
+        "url": "/consistency",
+        "teaser": null
+      },{
+        "title": "move your body every day",
+        "excerpt":" Move your body every day. Benefits include:      Improved sleep quality   Less risk of chronic disease   Increased productivity   Reduced anxiety   The “every day” part is important, because consistency is key to most things worth doing.  ","categories": [],
+        "tags": [],
+        "url": "/move-your-body-every-day",
+        "teaser": null
+      },{
+        "title": "Your first seed",
+        "excerpt":"Welcome! This is your first note. You’ll find it in the notes/ directory. GSW as arrived and is now editing this home page note. He’s been busy. In fact, he’s made TWO notes and they are: My new digital garden and Another note And a wikilink note: A new file...","categories": [],
+        "tags": [],
+        "url": "/your-first-note",
+        "teaser": null
+      },{
+        "title": "안녕하세요",
+        "excerpt":" This template supports any language. Come as you are! :)   ","categories": [],
+        "tags": [],
+        "url": "/%EC%95%88%EB%85%95%ED%95%98%EC%84%B8%EC%9A%94",
+        "teaser": null
+      },]
